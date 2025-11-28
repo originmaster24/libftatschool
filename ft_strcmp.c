3b0292d5ce83041f02924aa6214f6d9fb460ee6a
Zhi Yu Zhu <zzhu@c1r4p4.42firenze.it>
2025-11-28 11:59:17 +0100

    strcmp has some mistake, execute the code

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,47 +6,53 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 11:16:54 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/28 11:47:22 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/28 11:58:33 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-int	ft_strcmp(const char *s1, const char *s2);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int	ft_strcmp(const char *s1, const char *s2)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	index;
 
 	index = 0;
-	while ((s1[index] && s2[index]) && (s1[index + 1] != '\0'))
-		index++;
-	printf("%c,%c\n", s1[index], s2[index]);
+	while ((s1[index] && s2[index]) && (s1[index + 1] != '\0' && n > 0))
+		{
+			index++;
+			n--;
+		}
+	//printf("%c,%c\n", s1[index], s2[index]);
 	return ((int)((int)(s1[index]) - (int)(s2[index])));
 }
 
-// int main(void)
-// {
-// 	char str1[] = "ABC";
-// 	char str2[] = "AB";
-// 	char str3[] = "ABZ";
-// 	char str4[] = "ABC";
-// 	char str5[] = "ABJ";
-// 	char str6[] = "ABA";
+int main(void)
+{
+	char str1[] = "ABC";
+	char str2[] = "AB";
+	char str3[] = "ABZ";
+	char str4[] = "ABC";
+	char str5[] = "ABJ";
+	char str6[] = "ABA";
+	int n = 0;
+	
+	for (;n < 6; n++)
+	{
+		printf("n %d, str1 %s, str2 %s, orig: %d, ft: %d\n",
+			n, str1, str4, strncmp(str1, str4, n), ft_strncmp(str1, str4, n));
 
-// 	printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
-// 		 str1, str4, strcmp(str1, str4), ft_strcmp(str1, str4));
+		printf("n %d, str1 %s, str2 %s, orig: %d, ft: %d\n",
+			n, str1, str2, strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
 
-// 	printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
-// 		 str1, str2, strcmp(str1, str2), ft_strcmp(str1, str2));
+		printf("n %d, str1 %s, str2 %s, orig: %d, ft: %d\n",
+			n, str6, str3, strncmp(str6, str3, n), ft_strncmp(str6, str3, n));
 
-// 	printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
-// 		 str6, str3, strcmp(str6, str3), ft_strcmp(str6, str3));
-
-// 	printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
-// 		str5, str1, strcmp(str5, str1), ft_strcmp(str5, str1));
-
-// 	// printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
-// 	// "\201", "A", strcmp("\201", "A"), ft_strcmp("\201", "A"));	
-// }
+		printf("n %d, str1 %s, str2 %s, orig: %d, ft: %d\n",
+			n, str5, str1, strncmp(str5, str1, n), ft_strncmp(str5, str1, n));
+	}
+	// printf("str1 %s, str2 %s, orig: %d, ft: %d\n",
+	// "\201", "A", strcmp("\201", "A"), ft_strcmp("\201", "A"));	
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 19:46:28 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/28 11:46:54 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/28 11:56:07 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
