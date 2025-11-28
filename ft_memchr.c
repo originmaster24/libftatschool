f41ed7e0c17a4d1a553a6cbfe29de982dee18224
Zhi Yu Zhu <zzhu@c2r4p1.42firenze.it>
2025-11-28 20:49:19 +0100

    memchr normok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 20:24:56 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/28 20:41:22 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/28 20:47:03 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	*ft_memchr(const void *s, int c, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t index;
+	size_t	index;
 
 	index = 0;
 	if (n == 0)
 		return (NULL);
 	while (((char *)s)[index] && n >= 1)
 	{
-		if(((char *)s)[index] == c)
+		if (((char *)s)[index] == c)
 			return ((void *)s + index);
 		index++;
 		n--;
@@ -32,13 +32,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	return (NULL);
 }
 
-int main(void)
-{
-	char str[] = "helloworld!";
+// int main(void)
+// {
+// 	char str[] = "helloworld!";
 
-	for (unsigned char i = 0; i <= 127 ; i++)
-		for(size_t n = 0; n <= 11; n++)
-			printf("match: %c ascii: %d n: %ld, orig: %p, ft: %p\n",
-				i, i, n, memchr(str, i, n), ft_memchr(str, i, n));
-	
-}
+// 	for (unsigned char i = 0; i <= 127 ; i++)
+// 		for(size_t n = 0; n <= 11; n++)
+// 			printf("match: %c ascii: %d n: %ld, orig: %p, ft: %p\n",
+// 				i, i, n, memchr(str, i, n), ft_memchr(str, i, n));
+
+// }
