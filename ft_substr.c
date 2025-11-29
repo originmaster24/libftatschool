79198ea3aaf900a6116da48be9ec1fb64e40f308
Zhi Yu Zhu <zzhu@c1r4p4.42firenze.it>
2025-11-29 11:01:31 +0100

    substrnormok

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 10:43:36 by zzhu              #+#    #+#             */
+/*   Updated: 2025/11/29 11:00:48 by zzhu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t	n;
+
+	result = malloc(sizeof(char) * (len + 1));
+	n = ft_strlcpy(result, s + start, len + 1);
+	printf("%ld\n", n);
+	return (result);
+}
+
+// int	main(void)
+// {
+// 	char str[] = "helloworld!";
+// 	char *sub;
+
+// 	sub = ft_substr(str, 5, 5);
+// 	printf("%s\n", sub);
+// 	free(sub);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 19:46:28 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/28 22:45:13 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/29 10:44:55 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
