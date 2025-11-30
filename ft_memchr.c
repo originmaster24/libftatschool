122180d9feb05a614c0888752e937ff2e3e7766a
Zhi Yu Zhu <zzhu@c1r4p4.42firenze.it>
2025-11-30 17:39:44 +0100

    corrections

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 20:24:56 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/28 20:47:03 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/30 17:15:50 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	index = 0;
 	if (n == 0)
-		return (NULL);
-	while (((char *)s)[index] && n >= 1)
 	{
-		if (((char *)s)[index] == c)
-			return ((void *)s + index);
+		return (NULL);
+	}
+	while (n > 0)
+	{
+		if (((unsigned char *)s)[index] == (unsigned char)c)
+			return ((void *)((unsigned char *)s + index));
 		index++;
 		n--;
 	}
@@ -34,11 +36,20 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 // int main(void)
 // {
-// 	char str[] = "helloworld!";
+// 	char s[] = {0, 1, 2, 3, 4, 5};
 
-// 	for (unsigned char i = 0; i <= 127 ; i++)
-// 		for(size_t n = 0; n <= 11; n++)
-// 			printf("match: %c ascii: %d n: %ld, orig: %p, ft: %p\n",
-// 				i, i, n, memchr(str, i, n), ft_memchr(str, i, n));
-
+// 	// for (unsigned char i = 0; i <= 127 ; i++)
+// 	// 	for(size_t n = 0; n <= 11; n++)
+// 	// 		printf("match: %c ascii: %d n: %ld, orig: %p, ft: %p\n",
+// 	// 			i, i, n, memchr(str, i, n), ft_memchr(str, i, n));
+// 	printf("%s\n",
+// 	(char *)ft_memchr(s, 0, 0));
+// 	printf("%s\n",
+// 	(char *)ft_memchr(s, 0, 1));
+// 	printf("%s\n",
+// 	(char *)ft_memchr(s, 2, 3));
+// 	printf("%s\n",
+// 	(char *)ft_memchr(s, 6, 6));
+// 	printf("%s\n",
+// 	(char *)ft_memchr(s, 2 + 256, 3));
 // }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 20:50:14 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/29 20:39:22 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/30 17:38:58 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	index = 0;
 	if (n == 0)
 		return (0);
-	while (((unsigned char *)s1)[index] && n >= 2)
+	while (((unsigned char *)s1) && index < n)
 	{
 		if (((unsigned char *)s1)[index] != ((unsigned char *)s2)[index])
 			break ;
@@ -35,8 +35,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 // int main(void)
 // {
-// 	char *str1 = "helloworld!";
-// 	char *str2 = "helloaworld";
+// 	char *str1 = "!";
+// 	char *str2 = "test";
 // 	size_t n = 0;
 
 // 	for(;n < 12; n++)
@@ -48,4 +48,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 // 	n = 2;
 // 	printf("ayoo n: %ld, orig: %d ft: %d \n",
 // 	n, memcmp(str1, str2, n), ft_memcmp(str1, str2, n));
+
+// 	//char s[] = {-128, 0, 127, 0};
+// 	//char sCpy[] = {-128, 0, 127, 0};
+// 	char s2[] = {0, 0, 127, 0};
+// 	char s3[] = {0, 0, 42, 0};
+// 	printf("yyy, orig: %d ft: %d \n",
+// 	 memcmp(s2, s3, 4), ft_memcmp(s2, s3, 4));
+
 // }
