59a9d890acc80aaf43369cf648ebfa75cee6279e
Zhi Yu Zhu <zzhu@c1r4p4.42firenze.it>
2025-11-30 12:40:16 +0100

    putnbr corrected

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 16:51:05 by zzhu              #+#    #+#             */
-/*   Updated: 2025/11/29 17:42:41 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/11/30 12:39:51 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,61 @@
 
 void	ft_putnbr_fd(int n, int fd);
 
+static char	*findstart(char *str)
+{
+	if (*str != '\0')
+		return (str);
+	while (*str == '\0')
+		str++;
+	return (str);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	nb;
+	long	nb;
+	char	str[15];
+	int		idx;
+	int		negative;
 
-	nb = n + '0';
-	write(fd, &nb, 1);
+	nb = n;
+	negative = 0;
+	idx = 13;
+	ft_memset(str, '\0', 15);
+	if (nb < 0)
+	{
+		nb *= -1;
+		negative = 1;
+	}
+	while (nb > 0)
+	{
+		str[idx--] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	if (negative)
+		str[idx] = '-';
+	if (n == 0)
+		str[0] = '0';
+	ft_putstr_fd(findstart(str), fd);
 }
 
 // int main(void)
 // {
+// 	ft_putnbr_fd(0, 2);
+// 	printf("t1\n");
 // 	ft_putnbr_fd(5, 1);
+// 	printf("t2\n");
+// 	ft_putnbr_fd(-5, 2);
+// 	printf("t3\n");
+// 	ft_putnbr_fd(42, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(-57, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(164189, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(-987441, 2);
+// 	printf("\n");
+// 	ft_putnbr_fd(2147483647, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(-2147483648LL, 2);
+
 // }
